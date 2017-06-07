7fe409e4f22060abc4dcea35b1948cf8e6e8d1b7
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-06-07 16:15:51 +0200

    Added a tagline and styling in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,8 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="Header">
-      <h1 className="Header__headline">Desound</h1>
+      <h1 className="Header__headline">Desound.</h1>
+      <p className="Header__description">A place for discovering similar artists and more information about the bands you love.</p>
     </header>
   );
 };
