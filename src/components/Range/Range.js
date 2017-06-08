d21fe8e9f6f320395101fcf73d47f5d2c53c3871
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-06-08 09:12:29 +0200

    Added the component range to enable adjusting the number of search results

---
+++ src/components/Range/Range.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Range.css';
+
+class Range extends Component {
+  timeout = null;
+
+  render()  {
+    const { name, value, handleInput, handleChange } = this.props;
+    const onchange = (e) => {
+      handleInput(e);
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        handleChange(e);
+      }, 450 );
+    };
+
+    return (
+      <input className="range" type="range" name={ name } onChange={ onchange } value={ value } min="1" max="50" />
+    );
+  }
+}
+
+export default Range;
