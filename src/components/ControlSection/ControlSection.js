1c912a7da7024c873296e29dbd54d72417269d7f
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-06-10 11:32:26 +0200

    Renamed and cleaned up in ControlSection component

--- src/components/ControlSection/ControlSection.js
+++ src/components/ControlSection/ControlSection.js
@@ -3,14 +3,14 @@ import './ControlSection.css';
 import Range from '../Range/Range';
 import Select from '../Select/Select';
 
-const ControlSection = ({ limit, updateState, fetchData, method }) => {
+const ControlSection = ({ fetchData, limit, method, updateState }) => {
   return (
-    <div className="search-options"> { /* This should be a component */ }
-      <div className="search-options__item">
+    <div className="control-section">
+      <div className="control-section__item">
         <h2>Showing <span className="number">{ limit }</span> results</h2>
         <Range name="limit" value={ limit } handleInput={ updateState } handleChange={ fetchData } />
       </div>
-      <div className="search-options__item">
+      <div className="control-section__item">
         <h2>Search Result Showing</h2>
         <Select selected={ method } handleInput={ updateState } handleChange={ fetchData } />
       </div>
