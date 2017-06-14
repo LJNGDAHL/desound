c5dfd8c0eb3e868a44007ae8f0cadef6fafe925d
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-06-14 14:08:30 +0200

    Styled navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -27,6 +27,7 @@ class Navigation extends Component {
           <span></span>
         </div>
         <nav className={ navStyles }>
+          <h2 className="navigation__headline">Desound.</h2>
           <NavLink className="navigation__link" exact to="/" onClick={ this.toggleMenu }>Discover</NavLink>
           <NavLink className="navigation__link" to="/about" onClick={ this.toggleMenu }>About Desound</NavLink>
           <NavLink className="navigation__link" to="/lastfm" onClick={ this.toggleMenu }>Why Last.fm?</NavLink>
