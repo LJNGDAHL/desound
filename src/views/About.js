56fff0db9b26196b5a8aaf832592dfd96ef07b5c
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-06-14 14:22:27 +0200

    Continued with overall styling

--- src/views/About.js
+++ src/views/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return(
     <main className="app__content app__content--text">
       <h2>What is Desound?</h2>
-      <p>Desound is a place where you can find out more about the bands that you love. It is powered by Last.fm, a playground for music lovers from all over the world. Desound is build with React as a learning experience in a javascript course held at Medieinstitutet in Stockholm. If you want to have a look behind the scenes, the project can be found on <a className="app__link" href="https://github.com/LJNGDAHL/desound">Github</a>.</p>
+      <p>Desound is a place where you can find out more about the bands that you love. It is powered by Last.fm, a playground for music lovers from all over the world. Desound is build with React as a learning experience in a javascript course held at Medieinstitutet in Stockholm. If you want to have a look behind the scenes, the project can be found on <a className="link link--pink" href="https://github.com/LJNGDAHL/desound">Github</a>.</p>
     </main>
   );
 };
--- src/views/Lastfm.js
+++ src/views/Lastfm.js
@@ -5,7 +5,7 @@ const Lastfm = () => {
       <main className="app__content app__content--text">
         <h2>Why Last.fm?</h2>
         <p>Before there was Spotify, it was Last.fm. At least for me. Last.fm was a playground for music lovers all over the world. We were eager to find new bands similar to those we already adored. Last.fm helped you find music shows, new favorite songs and other people like you.</p>
-        <p>Also, the Last.fm API does not require an OAuth access token. For more information about Last.fm, visit Last.fm.</p>
+        <p>The Last.fm API does not require an OAuth access token. If you want to know more about how the API works, you can visit <a className="link link--pink" href="http://www.last.fm/api">Last.fm</a>.</p>
       </main>
   );
 };
